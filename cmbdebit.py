d69eb54a7077d4a750610783fc99dfdd49cd83c5
Shell.Xu <shell909090@gmail.com>
2021-01-28 12:16:36 +0800

    some details of cmbdebit

--- cmbdebit.py
+++ cmbdebit.py
@@ -31,17 +31,36 @@ def linefilter(src):
         yield line.replace('\t', '')
 
 
+# 信用卡存款, 信用卡还款
+# 朝朝宝转入, 朝朝宝转出
+# 黄金交易
+# 债券买卖
+# 受托申购-买理财, 受托分红-分红, 备注格式-产品ID,日期/流水号
+# 基金申购-买基金, 备注格式-基金ID
+# 结售汇即时售汇-换外汇
+# 国际结算费用-国际汇费
+# 转账汇款-汇款, 行内汇入-本行汇入, 客户转账-外行转账
+# U1PY/N5CP-财付通-微信支付
+# 线上支付-闪付
+non_types = {
+    '信用卡存款', '信用卡还款', '朝朝宝转入', '黄金交易', '债券买卖',
+    '受托申购', '基金申购', '结售汇即时售汇'}
+
 def main():
     ''' python3 cmb2csv.py [file]'''
     writer = csv.writer(sys.stdout)
 
-    with gzip.open(sys.argv[2], 'rt', encoding='utf-8') as fi:
+    with gzip.open(sys.argv[1], 'rt', encoding='gbk') as fi:
         for row in csv.reader(linefilter(fi)):
             if row[0] == '交易日期':
                 continue
-            if not row[3] or row[5] in ('信用卡存款', '信用卡还款', '受托申购', '结售汇即时售汇'):
+            if not row[3] or row[5] in non_types:
                 continue
-            if row[6].startswith('转账'):
+            # "转账汇款"或"客户转账"类别的支出，有时备注没有目标名称
+            # 只有"转账"二字，这种都是转给自己的
+            # 有时是"转账"+自己的名字
+            # 这两种统一处理
+            if row[5] in {'转账汇款', '客户转账'} and row[6].startswith('转账'):
                 continue
             writer.writerow([row[0][:6], row[0], cardnum, row[3], row[6]])
 
